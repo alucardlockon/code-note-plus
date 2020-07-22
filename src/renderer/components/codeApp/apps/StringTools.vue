8cac3ffe805a546106194a7550c7b0cca61783b8
alucardlockon <alucardlockon3@gmail.com>
2020-07-22 23:17:46 +0800

    添加字符串工具

--- src/renderer/components/codeApp/apps/StringTools.vue
+++ src/renderer/components/codeApp/apps/StringTools.vue
@@ -1,11 +1,11 @@
 <template>
    <div>
-      字符串:
+      字符串(a):
       <el-input v-model="stringParam"></el-input>
-      处理方式:
-      <el-input></el-input>
-      结果:
-      <el-input v-model="stringResult"></el-input>
+      处理方式(js代码,like a.split(',')):
+      <el-input v-model="code"></el-input>
+      结果(r):
+      <el-input :value="stringResult"></el-input>
    </div>
 </template>
 
@@ -15,7 +15,24 @@
       data: function () {
         return {
           stringParam: '',
-          stringResult: ''
+          code: ''
+        }
+      },
+      computed: {
+        stringResult: function () {
+          let result = ''
+          if (this.code) {
+            try {
+              // eslint-disable-next-line no-new-func
+              const func = new Function('a', 'return ' + this.code)
+              // eslint-disable-next-line no-useless-call
+              result = func.apply(null, [this.stringParam])
+            } catch (e) {
+              result = ''
+              console.error(e)
+            }
+          }
+          return result
         }
       }
     }
